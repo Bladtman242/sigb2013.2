bb480f659c33f8c06c3474ae34e8c37ac489170d
ungeroed <morten@fam-f.dk>
2013-04-13 14:55:15 +0200

    crossroads

--- SecondHalf.py
+++ SecondHalf.py
@@ -34,11 +34,29 @@ def cube_points(c,wid):
     p.append([c[0]+wid,c[1]-wid,c[2]-wid])
     return np.array(p).T
 
+
+def getCornerCoords(boardImg):
+
+    """
+
+    :param boardImg:
+    :return: cornercoords and centercoord
+    """
+    patternSize = (9,6)
+    found,corners=cv2.findChessboardCorners(boardImg, patternSize)
+    (x1,y1) = (corners[0][0][0],corners[0][0][1])
+    (x2,y2) = (corners[8][0][0],corners[8][0][1])
+    (x3,y3) = (corners[45][0][0],corners[45][0][1])
+    (x4,y4) = (corners[53][0][0],corners[53][0][1])
+    cubeCenterCoord = (((x1+x2)/2),((y1+y3)/2),10)
+    return [(x1,y1),(x2,y2),(x3,y3),(x4,y4)]
+
 def AugmentImages():
 
 
     I1 = cv2.imread("CalibrationImage1.jpg")
     I1Gray = cv2.cvtColor(I1,cv2.COLOR_RGB2GRAY)
+
     I2 = cv2.imread("CalibrationImage2.jpg")
     I2Gray = cv2.cvtColor(I2,cv2.COLOR_RGB2GRAY)
     I3 = cv2.imread("CalibrationImage3.jpg")
@@ -48,53 +66,58 @@ def AugmentImages():
     I5 = cv2.imread("CalibrationImage5.jpg")
     I5Gray = cv2.cvtColor(I5,cv2.COLOR_RGB2GRAY)
 
-    patternSize = (9,6)
-
-    found,corners=cv2.findChessboardCorners(I1Gray, patternSize)
-    (x1,y1) = (corners[0][0][0],corners[0][0][1])
-    (x2,y2) = (corners[8][0][0],corners[8][0][1])
-    (x3,y3) = (corners[45][0][0],corners[45][0][1])
-    (x3,y4) = (corners[53][0][0],corners[53][0][1])
-    global cubeCenterCoord
-    cubeCenterCoord = (((x1+x2)/2),((y1+y3)/2),100)
-
-    if (found!=0):
-        # cv2.drawChessboardCorners(I1, patternSize, corners,found)
-        cv2.circle(I1,(corners[45][0][0],corners[45][0][1]),5,(255,0,0))
+    I1Corners = getCornerCoords(I1)
+    I2Corners = getCornerCoords(I2)
 
 
+    H = estimateHomography(I1Corners, I2Corners)
 
     K, dist_coefs = calibrate.loadMatrixes()
 
-
-
-    box = cube_points(cubeCenterCoord,200)
+    box = cube_points((0,-0.2,0),0.2)
 
     # project bottom square in first image
 
     #This is K * [I | O]
     cam1 = Camera(hstack((K,dot(K,array([[0],[0],[-1]])) )) )
 
-    
+    box_cam1 = cam1.project(toHomogenious(box))
 
-    # box_cam1 = cam1.project(make_homog(box[:,:5]))
 
-    box_cam1 = cam1.project(toHomogenious(box[:,:5]))
 
     # 2D projection of bottom square
+    # figure()
+    # imshow(I1)
+    # plot(box_cam1[0,:],box_cam1[1,:],linewidth=3)
+    # show()
+
+
+
+    # print shape(box_cam1)
+
+
+    # use H to transfer points to the second image
+    box_trans = normalize(dot(H,box_cam1))
+    # compute second camera matrix from cam1 and H
+    cam2 = Camera(dot(H,cam1.P))
+    A = dot(linalg.inv(K),cam2.P[:,:3])
+    cross(A[:,0],A[:,1])
+    print "success made a cross product"
+
+    
+    A = array([A[:,0],A[:,1],cross(A[:,0],A[:,1])]).T
+    cam2.P[:,:3] = dot(K,A)
+
+    # project with the second camera
+    box_cam2 = cam2.project(toHomogenious(box_trans))
+
     figure()
-    imshow(I1)
+    imshow(I2)
     plot(box_cam1[0,:],box_cam1[1,:],linewidth=3)
     show()
 
-
-
-
-    print box_cam1
-
-    # cv2.imshow("Calibrated Image with box",I1)
-    # cv2.waitKey(0)
 AugmentImages()
+
 
 
 # compute second camera matrix from cam1 and H
